d3beaa3c2abae644a5884293aca96fa8ca796ed3
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-09-15 00:00:33 +0300

    Added new problem

---
+++ SyntaxFunctionsAndStatements-Exercise/Fruit.js
@@ -0,0 +1,9 @@
+function fruit(type, weight, price){
+    let weightInKg = weight / 1000;
+    let totalPrice = weightInKg * price;
+
+    console.log(`I need $${totalPrice.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${type}.`);
+}
+
+fruit('orange', 2500, 1.80);
+fruit('apple', 1563, 2.35);
