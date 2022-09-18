c475846fb736729e9089e5a13b054fdc8e47296b
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-09-18 22:34:18 +0300

    Added new files

---
+++ Arrays-Lab/NegativePositiveNumbers.js
@@ -0,0 +1,15 @@
+function solve(inputArr){
+    let newArr = [];
+
+    for (let i = 0; i < inputArr.length; i++) {
+        if (inputArr[i] < 0) {
+            newArr.unshift(inputArr[i]);
+        } else{
+            newArr.push(inputArr[i]);
+        }
+    }
+
+    console.log(newArr.join('\n'));
+}
+
+solve([7, -2, 8, 9])
