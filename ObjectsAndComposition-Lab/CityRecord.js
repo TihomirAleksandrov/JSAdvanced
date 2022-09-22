fcaa01ecff5a58d07716aae5e88cb06eeee12ee0
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-09-22 16:28:30 +0300

    Added new files

---
+++ ObjectsAndComposition-Lab/CityRecord.js
@@ -0,0 +1,12 @@
+function solve(city, people, money){
+    return{
+        name: city,
+        population: people,
+        treasury: money
+    };
+}
+
+console.log(solve('Tortuga',
+7000,
+15000
+));
