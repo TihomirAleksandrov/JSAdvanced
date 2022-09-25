6dcb702794a432e27a262a876ccb14cc610a1a55
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-09-25 15:53:51 +0300

    Added new files

---
+++ ObjectsAndComposition-Exercise/Rectangle.js
@@ -0,0 +1,13 @@
+function rectangle(width, height, color){
+    return{
+        width: width,
+        height: height,
+        color: color[0].toUpperCase() + color.slice(1),
+        calcArea() {
+            return this.width * this.height;
+        }
+    }
+}
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.calcArea());
