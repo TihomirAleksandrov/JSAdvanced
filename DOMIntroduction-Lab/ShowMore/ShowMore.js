685f3e10182dd80d81c4fb527d076856402cd287
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-09-28 00:49:36 +0300

    Added new files

---
+++ DOMIntroduction-Lab/ShowMore/ShowMore.js
@@ -0,0 +1,8 @@
+function showMore(){
+    let current = document.getElementById('more');
+    current.style.display = 'none';
+
+    let replacement = document.getElementById('text');
+    replacement.style.display = 'inline';
+    
+}
