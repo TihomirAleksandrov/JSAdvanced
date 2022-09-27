0f2e38c98e808490826b4ed636d4b99170e5c021
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-09-27 19:07:32 +0300

    Added new files

---
+++ DOMIntroduction-Lab/SumNumbers/SumNumbers.js
@@ -0,0 +1,7 @@
+function sum(){
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+    let result = document.getElementById('sum');
+
+    result.value = num1 + num2;
+}
